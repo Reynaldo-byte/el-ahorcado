3bc63f86aad453b487f7ef611b07f4ec1c0766da
Reynaldo-byte <64993522+Reynaldo-byte@users.noreply.github.com>
2021-10-01 14:19:24 -0500

    Update codigo.js

--- js/codigo.js
+++ js/codigo.js
@@ -168,8 +168,8 @@ var ctx;
                 var imagen = new Image();
                 imagen.src = "../imagenes/ahorcado"+errores+".png";
                 imagen.onload = function(){
-                    ctx.clearRect(100,0,460,230);
-                    ctx.drawImage(imagen, 100, 0, 460, 230);
+                    ctx.clearRect(180,0,460,230);
+                    ctx.drawImage(imagen, 180, 0, 460, 230);
                 }
             }
             
