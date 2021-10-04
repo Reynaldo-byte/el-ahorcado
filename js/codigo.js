3973a0ace7cc485496017f1ef15224ea2034b213
Reynaldo-byte <64993522+Reynaldo-byte@users.noreply.github.com>
2021-10-04 07:23:57 -0500

    Update codigo.js

--- js/codigo.js
+++ js/codigo.js
@@ -272,12 +272,13 @@ function avanzar() {
     
         letras_array.length=0;
         errores=0;
+            palabras_array.shift();
             document.getElementById('lista').innerHTML= document.getElementById('lista').innerHTML+"<BR>"+palabras_array[0];
     if(palabras_array.length>1){
                 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
                 
-    palabras_array.shift();
+    
     
     inicio();
     }
